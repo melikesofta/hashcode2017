60407b28cc93e8e0a314e5affb9b2bbf868888c9
Ahmet Bingul <abingu13@ku.edu.tr>
2017-02-23 20:30:36 +0300

    base

---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+DATE=`date +%Y_%m_%d_%H_%M_%S`
+FILE="outputs/$DATE.txt"
+
+echo ">>>>CODE>>>>>" >> $FILE
+cat main.py >> $FILE
+echo "" >> $FILE
+echo "<<<<CODE<<<<<" >> $FILE
+echo "" >> $FILE
+echo "" >> $FILE
+
+
+echo ">>>>OUTPUT>>>>>" >> $FILE
+python main.py >> $FILE
+echo "" >> $FILE
+echo "<<<<OUTPUT<<<<<" >> $FILE
+echo "" >> $FILE
+echo "" >> $FILE
